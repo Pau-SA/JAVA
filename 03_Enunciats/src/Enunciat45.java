c2fca84e23dcd558c9abd614379de0d35c68a917
Pau-SA <paucoga911@gmail.com>
2022-01-19 11:10:31 +0100

    Acualitzacions 19/01/2022)

--- 03_Enunciats/src/Enunciat45.java
+++ 03_Enunciats/src/Enunciat45.java
@@ -4,7 +4,7 @@ public class Enunciat45 {
 
     static Scanner teclat = new Scanner(System.in);
 
-//PER COMPROBAR ELS NUMEROS
+    // PER COMPROBAR ELS NUMEROS
     static int demanaNombre(String frase) {
         int numeroLlegit = 0;
         boolean nombreIncorrecte = false;
@@ -23,9 +23,8 @@ public class Enunciat45 {
         return numeroLlegit;
     }
 
-
-//PER FER LA SUMA   
-    static int suma(int numero1, int numero2){
+    // PER FER LA SUMA
+    static int suma(int numero1, int numero2) {
         int operaciosuma = 0;
 
         operaciosuma = numero1 + numero2;
@@ -33,8 +32,8 @@ public class Enunciat45 {
         return operaciosuma;
     }
 
-//PER FER LA RESTA
-    static int resta(int numero1, int numero2){
+    // PER FER LA RESTA
+    static int resta(int numero1, int numero2) {
         int operacioresta = 0;
 
         operacioresta = numero1 - numero2;
@@ -42,8 +41,8 @@ public class Enunciat45 {
         return operacioresta;
     }
 
-//PER FER LA MULTIPLICACIO
-    static int multi(int numero1, int numero2){
+    // PER FER LA MULTIPLICACIO
+    static int multi(int numero1, int numero2) {
         int operaciomulti = 0;
 
         operaciomulti = numero1 * numero2;
@@ -51,45 +50,80 @@ public class Enunciat45 {
         return operaciomulti;
     }
 
-
-//PER FER LA DIVISIO
-    static float divi(int numero1, int numero2){
+    // PER FER LA DIVISIO
+    static float divi(int numero1, int numero2) {
         float operaciodivi = 0;
 
-        operaciodivi = numero1 / numero2;
+        boolean noDivisio = false;
 
+        if ((numero1 == 0) || (numero2 == 0)) {
+            noDivisio = false;
+            System.out.println("No es pot fer!! Has posat un 0!!");
+        }else{
+            noDivisio = true;
+            System.out.println("Torna a escollir numeros!");
+            
+        }
+
+        operaciodivi = numero1 / numero2;
         return operaciodivi;
     }
 
 
+    static int potencia(int numero1, int numero2) {
+        int numPotencia = 1;
+        if (numero1 > 0 && numero2 == 0) {
+            return numPotencia;
+        } else if (numero1 == 0 && numero2 >= 1) {
+            return 0;
+        } else {
+            for (int i = 1; i <= numero2; i++) {
+                numPotencia = numPotencia * numero1;
+            }
+            return numPotencia;
+        }
+    }
 
     public static void main(String[] args) {
-        int num1 = demanaNombre("Entra el numero 1: ");
-        int num2 = demanaNombre("Entra el numero 2: ");
+
         int resultatOperacioSuma;
         int resultatOperacioResta;
         int resultatOperacioMulti;
         float resultatOperacioDivi;
+        float resultatOperacioPotencia = 0;
 
+        int opcio = 0;
 
+        System.out.println("Per fer la suma escriu 1");
+        System.out.println("Per fer la resta escriu 2");
+        System.out.println("Per fer la multi escriu 3");
+        System.out.println("Per fer la divi escriu 4");
+        System.out.println("Per fer la potencia escriu 5");
+
+        System.out.print("Escriu: ");
+        opcio = teclat.nextInt();
+
+        int num1 = demanaNombre("Entra el numero 1: ");
+        int num2 = demanaNombre("Entra el numero 2: ");
         System.out.println("Has entrat el número " + num1 + " i el " + num2);
 
-
-//PER MOSTRAR EL RESULTAT DE LES FUNCIONS ANTERIORS
-        resultatOperacioSuma = suma(num1, num2);
-        System.out.println("El resultat de la suma es: " + resultatOperacioSuma);
-
-
-        resultatOperacioResta = resta(num1, num2);
-        System.out.println("El resultat de la resta es: " + resultatOperacioResta);
-
-
-        resultatOperacioMulti = multi(num1, num2);
-        System.out.println("El resultat de la multipicació es: " + resultatOperacioMulti);
-
-
-        resultatOperacioDivi = divi(num1, num2);
-        System.out.println("El resultat de la divisió es: " + resultatOperacioDivi);
+        // PER MOSTRAR EL RESULTAT DE LES FUNCIONS ANTERIORS
+        if (opcio == 1) {
+            resultatOperacioSuma = suma(num1, num2);
+            System.out.println("El resultat de la suma es: " + resultatOperacioSuma);
+        } else if (opcio == 2) {
+            resultatOperacioResta = resta(num1, num2);
+            System.out.println("El resultat de la resta es: " + resultatOperacioResta);
+        } else if (opcio == 3) {
+            resultatOperacioMulti = multi(num1, num2);
+            System.out.println("El resultat de la multipicació es: " + resultatOperacioMulti);
+        } else if (opcio == 4) {
+            resultatOperacioDivi = divi(num1, num2);
+            System.out.println("El resultat de la divisió es: " + resultatOperacioDivi);
+        } else if (opcio == 5) {
+            resultatOperacioPotencia = potencia(num1, num2);
+            System.out.println("El resultat de la divisió es: " + resultatOperacioPotencia);
+        }
 
     }
 }
