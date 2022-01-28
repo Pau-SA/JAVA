d9c958f53b7a0f2773d25cdfdc85cbc18bc4e24c
Pau-SA <paucoga911@gmail.com>
2022-01-28 16:12:08 +0100

    Act

---
+++ Fitxers Joan/fitxers/src/FITXERSJOAN/Enunciats.java
@@ -0,0 +1,89 @@
+package FITXERSJOAN;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Enunciats {
+    public static void main(String[] args) {
+        
+        Scanner teclat = new Scanner(System.in);
+        //#region primos;
+        /*
+        int nombreEntrat;
+        boolean esPrimer;
+
+        System.out.print("Entra un número: ");
+        nombreEntrat = teclat.nextInt();
+        System.out.println(nombreEntrat);
+        esPrimer = Funcions.esPrimo(nombreEntrat);
+
+        if (esPrimer == true) {
+            System.out.println("El nombre entrat " + nombreEntrat + " es primer");
+        } else {
+            System.out.println("El nombre entrat " + nombreEntrat + " no es primer");
+        }
+
+        // PER ESCRIURE AL FITXER nombres del 2 al 100, pero només els primers
+        FileWriter fitxer = null;
+        try {
+            fitxer = new FileWriter("fileout.txt", false);
+
+            for (int i = 2; i < 100; i++) {
+                if (Funcions.esPrimo(i)) {
+                    fitxer.write(i);
+                }
+            }
+
+            fitxer.close();
+            teclat.close();
+        } catch (Exception MissatgeError) {
+            System.out.println("Error inesperat. " + MissatgeError.getMessage());
+        }
+
+
+        */
+        //#endregion
+
+
+
+        // OMPLIR UNA MATRIU (files) I (columnes)
+        int files, columnes;
+        do {
+            System.out.print("Entra la quantitat de files: ");
+            files = teclat.nextInt();
+
+            System.out.print("Entra la quantitat de columnes: ");
+            columnes = teclat.nextInt();
+
+            if (((files < 0) || (columnes < 0))) {
+                System.out.println("ERROR, TONRA-HI");
+            }
+        } while (((files < 0) || (columnes < 0)));
+
+
+
+        int[][] matriu = Funcions.matriu(files,columnes);
+
+        for (int i = 0; i < matriu.length; i++) {
+            String text = "";
+            for (int j = 0; j < matriu[i].length; j++) {
+                text+=matriu[i][j];
+            }
+            
+            Funcions.writeLineOnFile(text, true, (i==0)?false:true);
+        }
+
+
+
+
+
+
+       
+        teclat.close();
+
+
+
+
+    }
+}
---
+++ Fitxers Joan/fitxers/src/FITXERSJOAN/activitatNoms.java
@@ -0,0 +1,47 @@
+package FITXERSJOAN;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class activitatNoms {
+    public static void main(String[] args) {
+
+        Scanner punter = null;
+        String linia = "";
+
+        try {
+            // Llegim el contingut del fitxer
+            System.out.println("... Llegim el contingut del fitxer ...");
+            // punter = new Scanner(enunciats);
+
+            linia = punter.nextLine(); // Salvem la linia dins d'un String
+            // System.out.println(linia); // Mostrem la linia
+
+            String array[];
+            array = linia.split("&");
+
+            String[] resultat = new String[array.length];
+
+            for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+                resultat[j] = array[i];
+            }
+
+            for (int i = 0; i < resultat.length; i++) {
+                System.out.println(resultat[i]);
+            }
+
+        } catch (Exception exepcio1) {
+            System.out.println("Missatge: " + exepcio1.getMessage());
+        } finally {
+            // Tanquem el fitxer tant si la lectura ha estat correcta o no
+            try {
+                if (punter != null)
+                    punter.close();
+            } catch (Exception exepcio2) {
+                System.out.println("Missatge 2: " + exepcio2.getMessage());
+            }
+        }
+
+    }
+}
